c001c02b13361307a1f2d3460770b88f6f51a76e
canptleon <ardakeyisoglu@gmail.com>
2021-03-03 16:40:26 +0300

    +0

---
+++ project1/project1/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project1.Models;
+
+namespace project1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public int Index3()
+        {
+            return 24;
+        }
+
+        public String Index4()
+        {
+            return "Bugün hava çok sooğuk.";
+        }
+
+        public IActionResult Index5()
+        {
+            var ktp = new List<Kitap>()
+            {
+                new Kitap()
+                {
+                    ID = 1,
+                    KitapAd = "80 Günde Devri Alem",
+                    Yazar = "Jules Verne"
+                },
+
+                new Kitap()
+                {
+                    ID = 2,
+                    KitapAd = "Kitap 2",
+                    Yazar = "Yazar 2"
+                },
+
+                new Kitap()
+                {
+                    ID = 3,
+                    KitapAd = "Kitap 3",
+                    Yazar = "Yazar 3"
+                },
+
+                new Kitap()
+                {
+                    ID = 4,
+                    KitapAd = "Kitap 4",
+                    Yazar = "Yazar 4"
+                },
+
+            };
+            return View(ktp);
+        }
+
+        public IActionResult Index6()
+        {
+            ViewBag.deger1 = "Kar yağmaya devam ediyor.";
+
+            return View();
+        }
+
+        public IActionResult Index7()
+        {
+            
+
+            return View();
+        }
+
+        public IActionResult Index8()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult Index9()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ project1/project1/Models/Kitap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project1.Models
+{
+    public class Kitap
+    {
+        public int ID { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
+    }
+}
